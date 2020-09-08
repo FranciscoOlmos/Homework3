fd293ccf563cc13ba2353fe583137002f4392471
FranciscoOlmos <45568140+FranciscoOlmos@users.noreply.github.com>
2020-09-08 18:09:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var wrapperEle = document.body.querySelector(".wrapper");
+var animals = ["dog", "fish", "cat", "shark", "cat", "dog"];
+
+for(var i=0;i<animals.length;i++){
+results(animals[i]);
+}
+
+function results(str){
+    var ele = document.createElement("div");
+  if(str==="cat"){
+    ele.innerHTML="I am a cat";
+  }
+  else if(str==="dog"){
+    ele.innerHTML= "borf borf";
+  }
+  else{
+    ele.innerHTML= "I'm an animal";
+  }
+  wrapperEle.appendChild(ele);
+}
